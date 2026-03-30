0dff36c4c13328dfd82c2ab8a31d1246425194c0
Lisec, George <glisec@iu.edu>
2026-03-30 10:46:54 -0400

    cache aside abstraction

--- src/controllers/matches.ts
+++ src/controllers/matches.ts
@@ -5,38 +5,13 @@ import type { Request, Response } from "express";
 import { redis } from "@redis";
 import { getParamHashKey } from "@utils";
 
-interface matchesResult {
-  data: RiotParticipantDto[];
-  isFromCache: boolean;
-}
-
 export const getRecentMatches = async (req: Request, res: Response) => {
-  let result: matchesResult = { data: [], isFromCache: false };
-
   const riotId = req.params.riotId as string;
   const count = Math.min(parseInt(req.params.count as string) || 20, 20);
 
   if (!riotId) throw new BadRequestError("Riot ID is required");
 
-  const hashkey = getParamHashKey(riotId);
-  const cachedData = await redis.get(hashkey);
-  const docArr = cachedData ? JSON.parse(cachedData) : [];
-
-  if (docArr && docArr.length) {
-    result.data = docArr;
-    result.isFromCache = true;
-  } else {
-    const recentMatches: RiotParticipantDto[] =
-      await returnRecentMatches(riotId);
-
-    if (!recentMatches) throw new NotFoundError("Recent matches not found");
-
-    if (recentMatches && recentMatches.length) {
-      redis.set(hashkey, JSON.stringify(recentMatches), "EX", 60);
-    }
-
-    result.data = recentMatches;
-  }
+  const result = await cacheAside(returnRecentMatches, riotId);
 
   return res.json(result);
 };
@@ -45,8 +20,8 @@ async function cacheAside<T>(
   fn: (requestParam: string) => Promise<T>,
   requestParam: string,
 ) {
-  let res: { data: T[]; isFromCache: boolean } = {
-    data: [],
+  let res: { data: T | null; isFromCache: boolean } = {
+    data: null,
     isFromCache: false,
   };
 
@@ -60,16 +35,12 @@ async function cacheAside<T>(
   } else {
     const result: T = await fn(requestParam);
 
-    if (!result) throw new NotFoundError();
-
     if (result) {
       redis.set(hashkey, JSON.stringify(result), "EX", 60);
     }
 
     res.data = result;
   }
-}
 
-console.log(
-  await cacheAside<RiotParticipantDto[]>(returnRecentMatches, "Georgie#EZLL"),
-);
+  return res;
+}
